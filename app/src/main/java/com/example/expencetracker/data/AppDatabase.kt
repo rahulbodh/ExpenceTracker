87956f645ceaf5b23ae14d9635025cfd1c2edba0
Rahul <rahulbodh819@gmail.com>
2025-02-16 23:20:40 +0530

    create AppDatabase

--- app/src/main/java/com/example/expencetracker/data/AppDatabase.kt
+++ app/src/main/java/com/example/expencetracker/data/AppDatabase.kt
@@ -1,4 +1,37 @@
 package com.example.expencetracker.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.expencetracker.data.dao.CategoryDao
+import com.example.expencetracker.data.dao.ExpenseDao
+import com.example.expencetracker.data.entities.Category
+import com.example.expencetracker.data.entities.Expense
+
+@Database(
+    entities = [Category::class, Expense::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun categoryDao(): CategoryDao
+    abstract fun expenseDao(): ExpenseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "expense_manager_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
